acf726c3a8fc998fbb647495277c4519ff7091fb
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2018-11-21 16:11:11 +0000

    FIxes Job interface

--- src/app/models/job.interface.ts
+++ src/app/models/job.interface.ts
@@ -1,20 +1,26 @@
 
 import {User} from './user.interface';
 export interface Job {
-    id:		    string,										
-	business: 	string,
-	description:string,
-	position:	string,			
-	pay:		number,		
-	isTemporanery?:	boolean,
-	lat?:		number,
-	lng?:		number,
-	date?:		Date,
-	expiring?:	Date,
-	Created_at?:number,
-	applicants?:	[string],
-}
+	Job_id?:string,
+    Creator:string
+    Created_at?:number,
+    Business:string,
+    Pic?:string,
+    Address?:string,
+    Lat?:number,
+    Long?:number,
+    Description?:string,
+    Position:string,
+    Category?:string,
+    PayPerHour?:number,
+    Pay?:string,
+    IsTemp:boolean,
+    ShiftStartAt?:string,
+    ShiftEndAt?:string,
+    Date?:string,
+    Hours?:number
+  }
+
 
 
 	
-	// applicants:	{User, Message[]}[] | []
