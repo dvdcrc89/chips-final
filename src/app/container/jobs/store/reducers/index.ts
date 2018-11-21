30aa1a0ae7c44fbe3f68f0778ee8a631faf32157
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2018-11-21 18:18:41 +0000

    As above

--- src/app/container/jobs/store/reducers/index.ts
+++ src/app/container/jobs/store/reducers/index.ts
@@ -14,14 +14,3 @@ export const getJobsState = createFeatureSelector<JobsMarketState>('jobs');
 
 export const getJobState = createSelector(getJobsState,(state:JobsMarketState)=> state.jobsState);
 
-export const getJobsEntities = createSelector(getJobState,fromJobs.getJobsEntities);
-export const getAllJobs = createSelector(
-    getJobsEntities,
-    (entities)=>{
-        return Object.keys(entities).map(id=>entities[id])
-    }
-    );
-export const getJobsPages = createSelector(getJobState,fromJobs.getJobsPages);
-
-export const getJobsLoaded = createSelector(getJobState,fromJobs.getJobsLoaded);
-export const getJobsLoading = createSelector(getJobState,fromJobs.getJobsLoading);
