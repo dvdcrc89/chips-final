12a77c9cdda8bbf4669872b81dd293cc1fdbf692
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2018-11-21 16:10:30 +0000

    Implements new selector for jobs entities

--- src/app/container/jobs/store/reducers/index.ts
+++ src/app/container/jobs/store/reducers/index.ts
@@ -2,18 +2,25 @@ import * as fromJobs  from './jobs.reducer'
 import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store'
 
 export interface JobsMarketState{
- jobs: fromJobs.JobState
+ jobsState: fromJobs.JobState
 }
 
 export const reducers: ActionReducerMap<JobsMarketState> = {
-    jobs: fromJobs.reducer
+    jobsState: fromJobs.reducer
 }
 
 
 export const getJobsState = createFeatureSelector<JobsMarketState>('jobs');
 
-export const getJobState = createSelector(getJobsState,(state:JobsMarketState)=> state.jobs);
+export const getJobState = createSelector(getJobsState,(state:JobsMarketState)=> state.jobsState);
 
-export const getAllJobs = createSelector(getJobState,fromJobs.getJobs);
+export const getJobsEntities = createSelector(getJobState,fromJobs.getJobsEntities);
+export const getAllJobs = createSelector(
+    getJobsEntities,
+    (entities)=>{
+        return Object.keys(entities).map(id=>entities[id])
+    }
+    );
+
 export const getJobsLoaded = createSelector(getJobState,fromJobs.getJobsLoaded);
 export const getJobsLoading = createSelector(getJobState,fromJobs.getJobsLoading);
