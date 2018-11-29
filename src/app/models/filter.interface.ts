a016008daee0e06f6904b982d195f9ff7d2d2e49
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2018-11-29 19:00:20 +0000

    Implements Filter interface

--- src/app/models/filter.interface.ts
+++ src/app/models/filter.interface.ts
@@ -1,14 +1,6 @@
 export interface Filter {
-    Creator?: 	    string,	
-	Location?: 	    string,			
-	Business?:	    string,
-	Distance?:	    number,
-	JobCategory?:	string,
-	MinimumPay?:	number,
-	Position?:	    string[],
-	isTemporanery?:	boolean,
-	isPermanent?:	boolean,
-	date?:		    Date,
-	jobIds?:	    string[]
+  category:string[],
+  type:string,
+  date?:Date
 
 }
