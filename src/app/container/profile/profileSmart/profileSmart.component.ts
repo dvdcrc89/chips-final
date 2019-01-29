fdc60e58e26a794a64702aa18db45d89372a1680
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-01-29 15:50:48 +0000

    Adds profile picture initials placeholder

--- src/app/container/profile/profileSmart/profileSmart.component.ts
+++ src/app/container/profile/profileSmart/profileSmart.component.ts
@@ -23,7 +23,9 @@ import { Component, OnInit } from "@angular/core";
       this.lastName="La Porca"
       this.intrests=["Waitress","Bartender","Receptionist"];
       this.bio="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-
+      if(this.profilePic.length<=0){
+        this.profilePic="https://ui-avatars.com/api/?name="+this.firstName+'+'+this.lastName+'&background=0D8ABC&color=fff&size=512';
+      }
     }
     changeInfo(values){
       this.firstName=values.firstName;
