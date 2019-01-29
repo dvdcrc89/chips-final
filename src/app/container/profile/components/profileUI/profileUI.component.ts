6d91fac00cd21d166349116d6f3d2639a947f738
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-01-29 11:44:23 +0000

    Implements dummy profile

---
+++ src/app/container/profile/components/profileUI/profileUI.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'profileUI',
+    templateUrl: './profileUI.component.html',
+    styleUrls: ['./profileUI.component.css'],
+    
+  })
+  export class ProfileUIComponent implements OnInit {
+  
+    
+    ngOnInit(){
+
+    }
+}
--- src/app/container/profile/profile.module.ts
+++ src/app/container/profile/profile.module.ts
@@ -1,17 +1,45 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profileSmart/profileSmart.component';
-
+import { ProfileUIComponent } from './components/profileUI/profileUI.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {
+  MatFormFieldModule,
+  MatMenuModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MAT_DATE_LOCALE,
+  MatSlideToggleModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatButtonToggleModule,
+  MatChipsModule
+} from '@angular/material';
 
 
 @NgModule({
     declarations: [
     ProfileComponent,
+    ProfileUIComponent
       
     
     ],
     imports:[
-
+        MatInputModule,
+        MatButtonModule,
+        MatProgressSpinnerModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule,
+        MatSlideToggleModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatButtonToggleModule,
+        MatChipsModule
     ],
     exports:[
         ProfileComponent,
