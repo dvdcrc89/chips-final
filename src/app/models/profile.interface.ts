5f5adbcdfb1634a494df6cca08f97ed30d3fd647
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-01-30 17:54:07 +0000

    Implements Profile interface

---
+++ src/app/models/profile.interface.ts
@@ -0,0 +1,10 @@
+export interface Profile{
+    username?:string,
+    firstName?:string,
+    lastName?:string,
+    bio?:string,
+    message?:string,
+    profilePic?:string,
+    intrests?:any,
+    coverPic?:string,
+}
