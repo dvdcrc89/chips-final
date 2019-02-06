1a2f1e855b2b23fa8c7762465bab3fad658674bf
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-02-06 17:04:14 +0000

    [STABLE][STORE] Adds username and files selectors

--- src/app/container/profile/store/selectors/profile.selector.ts
+++ src/app/container/profile/store/selectors/profile.selector.ts
@@ -2,13 +2,17 @@ import { createSelector } from "@ngrx/store";
 import * as fromRoot from '../../../../store'
 import * as fromFeature from '../reducers';
 import * as fromProfile from '../reducers/profile.reducers'
+import { environment } from "src/environments/environment";
 
 
 
 export const getAllUser = createSelector(fromFeature.getProfileState,fromProfile.getProfileAllUsers);
 
 export const getMySelf = createSelector(fromFeature.getProfileState,fromProfile.getMySelf);
+export const getMyUsername = createSelector(getMySelf,(myself):string=>{
+    return  myself.username;
 
+});
 export const getMyName = createSelector(getMySelf,(myself):string=>{
     return myself.firstName;
 });
@@ -19,14 +23,28 @@ export const getMyBio = createSelector(getMySelf,(myself):string=>{
     return myself.bio;
 });
 export const getMyBioArray = createSelector(getMySelf,(myself):string[]=>{
-    let bioArray= myself.bio ? myself.bio.split("<br>") : [""]
+    let bioArray = myself.bio ? myself.bio.split("<br>") : [""]
+    console.log(bioArray);
     return bioArray;
 });
-export const getMyIntrests = createSelector(getMySelf,(myself):string=>{
-    return myself.intrests;
+export const getMyIntrests = createSelector(getMySelf,(myself):string[]=>{
+    return myself.intrests  ? myself.intrests.split(','): [""];
 });
 export const getMyMessage = createSelector(getMySelf,(myself):string=>{
     return myself.message;
+});
+
+export const getMyPP = createSelector(getMySelf,(myself):string=>{
+    let username = myself.username;
+    return environment.s3_imgs+username+"_PP"+"?"+Math.random();;
+});
+export const getMyCP = createSelector(getMySelf,(myself):string=>{
+    let username = myself.username;
+    return environment.s3_imgs+username+"_CP"+"?"+Math.random();;
+});
+export const getMyCV = createSelector(getMySelf,(myself):string=>{
+    let username = myself.username;
+    return environment.s3_imgs+username+"_CV"+"?"+Math.random();;
 });
 
 export const getProfileToSee = createSelector(fromFeature.getProfileState,fromProfile.getProfileToSee);
