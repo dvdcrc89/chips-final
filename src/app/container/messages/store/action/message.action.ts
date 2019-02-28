51bdf139e63f660d5c70c24227ef9565d7fd739e
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-02-28 16:30:16 +0000

    [STORE] Implements action for message store

--- src/app/container/messages/store/action/message.action.ts
+++ src/app/container/messages/store/action/message.action.ts
@@ -1,4 +1,30 @@
 import {Action} from '@ngrx/store'
 import { Profile } from '../../../../models/profile.interface';
 
-export type MessageAction = {}
+export const LOAD_ALL_CONVERSATIONS = '[Conversation[]] Load All Conversation';
+export const LOAD_ALL_CONVERSATIONS_SUCCESS = '[Conversation[]] Load All Conversation Success';
+export const LOAD_ALL_CONVERSATIONS_FAIL = '[Conversation[]] Load All Conversation Fail';
+
+export class LoadAllConversation implements Action {
+    readonly type = LOAD_ALL_CONVERSATIONS;
+}
+export class LoadAllConversationfFail implements Action {
+    readonly type = LOAD_ALL_CONVERSATIONS_FAIL;
+    constructor(public payload: any){}
+}
+
+export class LoadAllConversationSuccess implements Action {
+    readonly type = LOAD_ALL_CONVERSATIONS_SUCCESS;
+    constructor(public payload: any[]){}
+
+}
+
+
+
+
+export type MessageAction = {
+
+ 
+    
+    
+}
