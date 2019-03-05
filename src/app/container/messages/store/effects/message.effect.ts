7b12f09e8b15cd263c90809ba8006e708f99b08e
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-03-05 16:27:04 +0000

    [STORE] Implements Message Store Effects

--- src/app/container/messages/store/effects/message.effect.ts
+++ src/app/container/messages/store/effects/message.effect.ts
@@ -2,9 +2,43 @@ import {Injectable} from '@angular/core';
 import {Effect, Actions} from '@ngrx/effects';
 import * as messageAction from '../action/message.action';
 import { map, switchMap, catchError } from 'rxjs/operators';
-// import * as fromService from '../../services/profile.service'
+import * as fromService from '../../services/message.service'
 import { of } from 'rxjs';
 
 @Injectable()
 export class MessageEffects{
+    constructor(
+        private actions$: Actions,
+        private messageService: fromService.MessageService
+        ) {}
+    
+    @Effect()
+    loadAllConversation$ = this.actions$.ofType(messageAction.LOAD_ALL_CONVERSATIONS)
+     .pipe(
+         switchMap(()=>{
+            return this.messageService.getAllMessages().pipe(
+                map(messages => new messageAction.LoadAllConversationSuccess(messages)),
+                catchError(error => of(new messageAction.LoadAllConversationFail(error)))          
+         )}
+     ))
+
+     @Effect()
+    send$ = this.actions$.ofType(messageAction.SEND_MESSAGE)
+     .pipe(
+         switchMap((action: messageAction.SendMessage)=>{
+            return this.messageService.sendMessage(action.payload).pipe(
+                map(res => new messageAction.SendMessageSuccess()),
+                catchError(error => of(new messageAction.SendMessageFail(error)))          
+         )}
+     ))
+     @Effect()
+     read$ = this.actions$.ofType(messageAction.READ_MESSAGE)
+     .pipe(
+         switchMap((action: messageAction.ReadMessage)=>{
+            return this.messageService.read(action.payload).pipe(
+                map(res => new messageAction.ReadMessageSuccess()),
+                catchError(error => of(new messageAction.ReadMessageFail(error)))          
+         )}
+     ))
+      
 }
