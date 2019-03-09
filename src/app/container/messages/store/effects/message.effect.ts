d1d702fb3d1a0429742c7aeecccf0d6f667f7219
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2019-03-09 13:01:40 +0000

    [STORE] Implements @Effect for url active conversation selector

--- src/app/container/messages/store/effects/message.effect.ts
+++ src/app/container/messages/store/effects/message.effect.ts
@@ -1,17 +1,20 @@
 import {Injectable} from '@angular/core';
 import {Effect, Actions} from '@ngrx/effects';
 import * as messageAction from '../action/message.action';
-import { map, switchMap, catchError } from 'rxjs/operators';
+import { map, switchMap, catchError, withLatestFrom } from 'rxjs/operators';
 import * as fromService from '../../services/message.service'
 import { of } from 'rxjs';
 import { AuthService } from 'src/app/container/user/auth.service';
+import * as fromRouter from '../../../../store';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class MessageEffects{
     constructor(
         private actions$: Actions,
         private messageService: fromService.MessageService,
-        private authService: AuthService
+        private authService: AuthService,
+        private storeRouter :Store<fromRouter.RouterStateUrl>
         ) {}
     
     @Effect()
@@ -53,6 +56,52 @@ export class MessageEffects{
                 catchError(error => of(new messageAction.ReadMessageFail(error)))          
          )}
      ))
-     
-      
+
+    //  @Effect()
+    //  setActive$ = this.actions$.ofType(messageAction.LOAD_ALL_CONVERSATIONS_SUCCESS)
+    //  .pipe(
+    //      switchMap((action: messageAction.LoadAllConversationSuccess)=>{
+    //         return this.storeRouter.select('routerReducer', 'state', 'params').pipe(
+    //             map(params => {
+    //                 console.log(params);
+    //                 if(params.username){
+    //                     let payload:any = {
+    //                       him:params.username
+    //                      };
+    //                     if(params.job_id) payload = {...payload, job_id:params.job_id}  
+    //                     console.log(payload);
+    //                      of(new messageAction.SetActiveConversation(payload))
+    //                     }}),
+    //                      catchError(error => of(new messageAction.ReadMessageFail(error)))          
+               
+    //         )}
+    //             ))
+                            
+                @Effect()
+                setActive$ = this.actions$.ofType(messageAction.LOAD_ALL_CONVERSATIONS_SUCCESS)
+                .pipe(
+                    withLatestFrom(
+                        this.storeRouter.select('routerReducer', 'state', 'params'),
+                        (routerReducer, params :any) => {
+                            console.log("routerReducer",routerReducer,"paramns",params);
+                            if(params.username){
+                                let payload:any = {
+                                  him:params.username
+                                 };
+                                if(params.job_id) payload = {...payload, jobs_id:params.job_id} 
+                            return  new messageAction.SetActiveConversation(payload)
+                                
+                            
+                        }
+                    }
+                    ))
+                //     switchMap(newPayload => {
+                //         return this.detailService
+                //         .getDetail(newPayload)
+                //         .pipe(
+                //             map(detail=> new DetailActions.GetDetailSuccess(detail)),
+                //             catchError(error => Observable.of(new DetailActions.GetDetailFail(error)))
+                //         );
+                //     })
+                // );
 }
